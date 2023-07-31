db7eef5aef1ca421de7ac1f497cf342c6551ea29
Subrahmanya Lingappa <slingappa@ventanamicro.com>
2023-07-31 17:20:30 +0530

    hw/riscv: Add RPMI Hart State Management handlers
    Signed-off-by: Subrahmanya Lingappa <slingappa@ventanamicro.com>

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -368,6 +368,8 @@ struct CPUArchState {
     hwaddr kernel_addr;
     hwaddr fdt_addr;
 
+    bool (*wfi_cb)(void *);
+
 #ifdef CONFIG_KVM
     /* kvm timer */
     bool kvm_timer_dirty;
@@ -407,6 +409,11 @@ struct ArchCPU {
 static inline int riscv_has_ext(CPURISCVState *env, target_ulong ext)
 {
     return (env->misa_ext & ext) != 0;
+}
+
+static inline void riscv_set_wfi_cb(CPURISCVState *env, void *cb)
+{
+    env->wfi_cb = cb ;
 }
 
 #include "cpu_user.h"
